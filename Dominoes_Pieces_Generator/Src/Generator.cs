5d778223a083af59eecefa67c65dce88ea6fe0b4
brbmendes <bbmendes@gmail.com>
2017-06-28 08:54:26 -0300

    Added excpetion at the beginning of the file and corrected if-else to generate pieces.

--- Dominoes_Pieces_Generator/Src/Generator.cs
+++ Dominoes_Pieces_Generator/Src/Generator.cs
@@ -1,7 +1,7 @@
 ﻿/// <summary>
 /// exe             : Dominoes_Pieces_Generator.exe
 /// Description     : Generate test files for Backtracking_Domino
-/// Usage           : In command line, run "Dominoes_Pieces_Generator.exe X Y", where X is the number of pieces to be generated and Y is the max value each side of pieces can reach.
+/// Usage           : In command line, run "Dominoes_Pieces_Generator.exe X Y", where X is the number of pieces to be generated and Y-1 is the max value each side of pieces can reach.
 /// Output          : A file with the number of pieces in the first line and the pieces randomly generated. 
 /// Author          : Bruno Bragança Mendes <bbmendes@gmail.com>
 /// Date            : Mon, 2017 Jun 26, 18:08:26 BRT
@@ -23,22 +23,19 @@ namespace Dominoes_Pieces_Generator.Src
                 int numberOfPiecesCopy = numberOfPieces;
                 int maxValueOfPieces = Convert.ToInt32(args[1]);
                 double maxPieces = Math.Pow(maxValueOfPieces, 2);
-                Boolean preventLoop = false;
                 Dictionary<Int32, String> pieces = new Dictionary<int, string>();
-                HashSet<String> controlPieces = new HashSet<String>();
                 Random rnd = new Random();
 
+                if(maxPieces < numberOfPieces)
+                {
+                    throw new ArgumentException("Impossible create file. Max number of combination values for pieces has been reached.");
+                }
+                
                 while (numberOfPiecesCopy > 0)
                 {
                     String line = String.Format("{0} {1}", rnd.Next(0, maxValueOfPieces), rnd.Next(0, maxValueOfPieces));
                     if (pieces.ContainsValue(line.ToString()))
                     {
-                        controlPieces.Add(line);
-                        if(controlPieces.Count == maxPieces)
-                        {
-                            preventLoop = true;
-                            break;
-                        }
                         Boolean hasAdded = false;
                         while (!hasAdded)
                         {
@@ -49,16 +46,6 @@ namespace Dominoes_Pieces_Generator.Src
                                 hasAdded = true;
                                 numberOfPiecesCopy--;
                             }
-                            else
-                            {
-                                controlPieces.Add(line);
-                                numberOfPiecesCopy--;
-                                if (controlPieces.Count == maxPieces)
-                                {
-                                    preventLoop = true;
-                                    break;
-                                }
-                            }
                         }
                     }
                     else
@@ -68,55 +55,49 @@ namespace Dominoes_Pieces_Generator.Src
                     }
                 }
 
-                if(preventLoop == true)
+                int numberPart = 1;
+                String caseName = String.Format("CASE_{0}.txt", numberPart.ToString().PadLeft(3, '0'));
+
+                if (File.Exists(caseName))
                 {
-                    Console.WriteLine("Impossible create file. Max number of combination values for pieces has been reached.");
+                    Boolean hasFile = true;
+                    while (hasFile)
+                    {
+                        numberPart++;
+                        caseName = String.Format("CASE_{0}.txt", numberPart.ToString().PadLeft(3, '0'));
+                        if (!File.Exists(caseName))
+                        {
+                            using (StreamWriter sw = File.AppendText(caseName))
+                            {
+                                sw.WriteLine(numberOfPieces);
+                                foreach (KeyValuePair<Int32, String> piece in pieces)
+                                {
+                                    sw.WriteLine(piece.Value);
+                                }
+                            }
+                            hasFile = false;
+                        }
+                    }
                 }
                 else
                 {
-                    int numberPart = 1;
-                    String caseName = String.Format("CASE_{0}.txt", numberPart.ToString().PadLeft(3, '0'));
-
-                    if (File.Exists(caseName))
+                    using (StreamWriter sw = File.AppendText(caseName))
                     {
-                        Boolean hasFile = true;
-                        while (hasFile)
+                        sw.WriteLine(numberOfPieces);
+                        foreach (KeyValuePair<Int32, String> piece in pieces)
                         {
-                            numberPart++;
-                            caseName = String.Format("CASE_{0}.txt", numberPart.ToString().PadLeft(3, '0'));
-                            if (!File.Exists(caseName))
-                            {
-                                using (StreamWriter sw = File.AppendText(caseName))
-                                {
-                                    sw.WriteLine(numberOfPieces);
-                                    foreach (KeyValuePair<Int32, String> piece in pieces)
-                                    {
-                                        sw.WriteLine(piece.Value);
-                                    }
-                                }
-                                hasFile = false;
-                            }
+                            sw.WriteLine(piece.Value);
                         }
                     }
-                    else
-                    {
-                        using (StreamWriter sw = File.AppendText(caseName))
-                        {
-                            sw.WriteLine(numberOfPieces);
-                            foreach (KeyValuePair<Int32, String> piece in pieces)
-                            {
-                                sw.WriteLine(piece.Value);
-                            }
-                        }
-                    }
-                    Console.WriteLine(String.Format("\nThe file has been created: {0}", caseName));
                 }
+                Console.WriteLine(String.Format("\nThe file has been created: {0}", caseName));
 
                 Console.ReadKey();
 
             } catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                Console.ReadKey();
             }
         }
     }
