352fecc29049985fec3860c261d5274fef1e31ae
brbmendes <bbmendes@gmail.com>
2017-06-28 09:24:10 -0300

    Added elapsed time to generate pieces and file.

--- Dominoes_Pieces_Generator/Src/Generator.cs
+++ Dominoes_Pieces_Generator/Src/Generator.cs
@@ -5,10 +5,11 @@
 /// Output          : A file with the number of pieces in the first line and the pieces randomly generated. 
 /// Author          : Bruno Bragança Mendes <bbmendes@gmail.com>
 /// Date            : Mon, 2017 Jun 26, 18:08:26 BRT
-/// Version         : 1.0
+/// Version         : 1.1
 /// </summary>
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace Dominoes_Pieces_Generator.Src
@@ -25,12 +26,14 @@ namespace Dominoes_Pieces_Generator.Src
                 double maxPieces = Math.Pow(maxValueOfPieces, 2);
                 Dictionary<Int32, String> pieces = new Dictionary<int, string>();
                 Random rnd = new Random();
+                Stopwatch swatch = new Stopwatch();
 
                 if(maxPieces < numberOfPieces)
                 {
                     throw new ArgumentException("Impossible create file. Max number of combination values for pieces has been reached.");
                 }
-                
+
+                swatch.Start();
                 while (numberOfPiecesCopy > 0)
                 {
                     String line = String.Format("{0} {1}", rnd.Next(0, maxValueOfPieces), rnd.Next(0, maxValueOfPieces));
@@ -54,7 +57,15 @@ namespace Dominoes_Pieces_Generator.Src
                         numberOfPiecesCopy--;
                     }
                 }
+                swatch.Stop();
 
+                Console.WriteLine("#################################################################");
+                Console.WriteLine("Metrics:");
+                Console.WriteLine("Elapsed time to generate {0} pieces: \t {1}", numberOfPieces, swatch.Elapsed.ToString());
+                Console.WriteLine("#################################################################\n\n");
+                swatch.Reset();
+
+                swatch.Start();
                 int numberPart = 1;
                 String caseName = String.Format("CASE_{0}.txt", numberPart.ToString().PadLeft(3, '0'));
 
@@ -90,7 +101,14 @@ namespace Dominoes_Pieces_Generator.Src
                         }
                     }
                 }
-                Console.WriteLine(String.Format("\nThe file has been created: {0}", caseName));
+                swatch.Stop();
+
+                Console.WriteLine(String.Format("\nThe file has been created: {0}\n\n\n", caseName));
+
+                Console.WriteLine("#################################################################");
+                Console.WriteLine("Metrics:");
+                Console.WriteLine("Elapsed time to generate '{0}' file: \t {1}", caseName, swatch.Elapsed.ToString());
+                Console.WriteLine("#################################################################");
 
                 Console.ReadKey();
 
